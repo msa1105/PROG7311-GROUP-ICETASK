9143b59208e43be85a01ec849053806edfc6bbcd
ZiyaadSimjee <ST10406906@vcconnect.edu.za>
2026-05-05 22:20:30 +0200

    ITransactionService.cs added

--- Interfaces/ITransactionService.cs
+++ Interfaces/ITransactionService.cs
@@ -1,10 +1,20 @@
-using System.Collections.Generic;   
+using System.Collections.Generic;
 using FinanceTrack.Models;
 
 namespace FinanceTrack.Interfaces
 {
     public interface ITransactionService
     {
+        IEnumerable<Transaction> GetAllTransactions();
+        Transaction GetTransactionById(int id);
+        void AddTransaction(Transaction transaction);
+        void UpdateTransactionStatus(int id, string targetStatus);
 
+        // Specific queries for the dashboard and views
+        decimal GetTotalBalance();
+        decimal GetNetProfit();
+        decimal GetTotalAssets();
+        decimal GetTotalLiabilities();
+        IEnumerable<Transaction> GetRecentTransactions(int count = 10);
     }
 }
